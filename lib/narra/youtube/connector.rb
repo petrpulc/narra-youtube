4d681472e41865555d5acb30942fe7828abfe449
kubinpe5 <kubinpe5@fit.cvut.cz>
2015-04-12 14:43:16 +0200

    Opraveno testování a dokonalý reg exp pro URL

--- lib/narra/youtube/connector.rb
+++ lib/narra/youtube/connector.rb
@@ -19,7 +19,10 @@
 # Authors: Petr Kubin
 #
 
-require 'narra/spi'
+require 'narra/core'
+require 'net/http'
+require 'rubygems'
+require 'json'
 
 module Narra
   module Youtube
@@ -27,100 +30,84 @@ module Narra
 
       @identifier = :youtube
       @title = 'NARRA YouTube Connector'
-      @self_description = 'Allows NARRA to connects to the YouTube sources'
+      @description = 'Allows NARRA to connects to the YouTube sources'
 
       def self.valid?(url)
         # regular expression of youtube url - validation test
-        !!(url =~ /^(http:\/\/|https:\/\/)?(www\.)?(youtube\.[A-Za-z\.]+|youtu.be\.)\/(watch\?v=)?[A-Za-z0-9\-_]{6,12}(&[A-Za-z0-9\-_]+)*=?.*$/)
+        !!(url =~ /^(http:\/\/|https:\/\/)?(www\.)?(youtu\.be\/|youtube\.com\/(embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){6,11})(?:\S+)?$/)
       end
 
-      def initialization(url)
+      def initialize(url)
         # all description from YouTube API
         pom = url.split('v=')
         @videoid = pom[1].split('&')[0]
-
-        @youtube = get 'https://www.googleapis.com/youtube/v3/videos?id=#{@videoid}&key=AIzaSyBVYtP85g7VCilGKbzkQqPCf8CxokAfvhU&part=snippet,statistics,contentDetails'
+        uri = URI("https://www.googleapis.com/youtube/v3/videos?id=#{@videoid}&key=AIzaSyBVYtP85g7VCilGKbzkQqPCf8CxokAfvhU&part=snippet,statistics,contentDetails,status")
+        @youtube = Net::HTTP.get(uri)
 
       end
 
       def name
         # jmeno video na youtube | title
-        pom = @youtube.split('"title": "')[1]
-        @name = pom.split("\",\n")[0]
+        my_hash = JSON.parse(@youtube)
+        my_hash["items"][0]["snippet"]["title"]
       end
 
       def type
         :video
       end
 
-      # zeptat se na přetížení methody a parametrizaci
+      # Method to return metadata as map
       def metadata
-        # parse youtube_json_object with variables bellow
+        my_hash = JSON.parse(@youtube)
         #channelId
-        pom = @youtube.split('"channelId": "')[1]
-        @channelId = pom.split("\",\n")[0]
+        @channelId = my_hash["items"][0]["snippet"]["channelId"]
         #channelTitle
-        pom = @youtube.split('"channelTitle": "')[1]
-        @channelTitle = pom.split("\",\n")[0]
+        @channelTitle = my_hash["items"][0]["snippet"]["channelTitle"]
         #id
-        pom = @youtube.split('"id": "')[1]
-        @id = pom.split("\",\n")[0]
+        @id = my_hash["items"][0]["snippet"]["id"]
         #publishedAt
-        pom = @youtube.split('"publishedAt": "')[1]
-        @publishedAt = pom.split("\",\n")[0]
+        @publishedAt = my_hash["items"][0]["snippet"]["publishedAt"]
         #description
-        pom = @youtube.split('"description": "')[1]
-        @description = pom.split("\",\n")[0]
+        @my_description = my_hash["items"][0]["snippet"]["description"]
         #categoryId
-        pom = @youtube.split('"categoryId": "')[1]
-        @categoryId = pom.split("\",\n")[0]
+        @categoryId = my_hash["items"][0]["snippet"]["categoryId"]
         #liveBroadcastContent
-        pom = @youtube.split('"liveBroadcastContent": "')[1]
-        @liveBroadcastContent = pom.split("\",\n")[0]
+        @liveBroadcastContent = my_hash["items"][0]["snippet"]["liveBroadcastContent"]
         #viewCount
-        pom = @youtube.split('"viewCount": "')[1]
-        @viewCount = pom.split("\",\n")[0]
+        @viewCount = my_hash["items"][0]["statistics"]["viewCount"]
         #likeCount
-        pom = @youtube.split('"likeCount": "')[1]
-        @likeCount = pom.split("\",\n")[0]
+        @likeCount = my_hash["items"][0]["statistics"]["likeCount"]
         #dislikeCount
-        pom = @youtube.split('"dislikeCount": "')[1]
-        @dislikeCount = pom.split("\",\n")[0]
+        @dislikeCount = my_hash["items"][0]["statistics"]["dislikeCount"]
         #favouriteCount
-        pom = @youtube.split('"favoriteCount": "')[1]
-        @favoriteCount = pom.split("\",\n")[0]
+        @favoriteCount = my_hash["items"][0]["statistics"]["favouriteCount"]
         #commentCount
-        pom = @youtube.split('"commentCount": "')[1]
-        @commentCount = pom.split("\"\n")[0]
+        @commentCount = my_hash["items"][0]["statistics"]["commentCount"]
         #duration
-        pom = @youtube.split('"duration": "')[1]
-        @duration = pom.split("\",\n")[0]
+        @duration = my_hash["items"][0]["contentDetails"]["duration"]
         #dimension
-        pom = @youtube.split('"dimension": "')[1]
-        @dimension = pom.split("\",\n")[0]
+        @dimension = my_hash["items"][0]["contentDetails"]["dimension"]
         #definition
-        pom = @youtube.split('"definition": "')[1]
-        @definition = pom.split("\",\n")[0]
+        @definition = my_hash["items"][0]["contentDetails"]["definition"]
         #caption
-        pom = @youtube.split('"caption": "')[1]
-        @caption = pom.split("\",\n")[0]
+        @caption = my_hash["items"][0]["contentDetails"]["caption"]
 
-        data = Array[ {name:'channelId', value:'#{@channelId}'},
-                      {name:'channelTitle', value:'#{@channelTitle}'},
-                      {name:'publishedAt', value:'#{@publishedAt}'},
-                      {name:'description', value:'#{@descriprion}'},
-                      {name:'categoryId', value:'#{@categoryId}'},
-                      {name:'liveBroadcastContent', value:'#{@liveBroadcastContent}'},
-                      {name:'viewCount', value:'#{@viewCount}'},
-                      {name:'likeCount', value:'#{@likeCount}'},
-                      {name:'dislikeCount', value:'#{@dislikeCount}'},
-                      {name:'favouriteCount', value:'#{@favouriteCount}'},
-                      {name:'commentCount', value:'#{@commentCount}'},
-                      {name:'duration', value:'#{@duration}'},
-                      {name:'dimension', value:'#{@dimension}'},
-                      {name:'definition', value:'#{@definition}'},
-                      {name:'caption', value:'#{@caption}'}
-                    ]
+        data = Array[ {name:'channelId', value:"#{@channelId}"},
+                      {name:'channelTitle', value:"#{@channelTitle}"},
+                      {name:'publishedAt', value:"#{@publishedAt}"},
+                      {name:'description', value:"#{@my_description}"},
+                      {name:'categoryId', value:"#{@categoryId}"},
+                      {name:'liveBroadcastContent', value:"#{@liveBroadcastContent}"},
+                      {name:'viewCount', value:"#{@viewCount}"},
+                      {name:'likeCount', value:"#{@likeCount}"},
+                      {name:'dislikeCount', value:"#{@dislikeCount}"},
+                      {name:'favouriteCount', value:"#{@favouriteCount}"},
+                      {name:'commentCount', value:"#{@commentCount}"},
+                      {name:'duration', value:"#{@duration}"},
+                      {name:'dimension', value:"#{@dimension}"},
+                      {name:'definition', value:"#{@definition}"},
+                      {name:'caption', value:"#{@caption}"}
+        ]
       end
 
       def download_url
--- spec/youtube/connector_spec.rb
+++ spec/youtube/connector_spec.rb
@@ -24,7 +24,7 @@ require 'spec_helper'
 describe Narra::Youtube::Connector do
   before(:each) do
     # test url
-    @url = 'http://www.youtube.com'
+    @url = 'https://www.youtube.com/watch?v=qM9f01YYDJ4'
   end
 
   it 'can be instantiated' do
@@ -34,7 +34,7 @@ describe Narra::Youtube::Connector do
   it 'should have accessible fields' do
     expect(Narra::Youtube::Connector.identifier).to match(:youtube)
     expect(Narra::Youtube::Connector.title).to match('NARRA YouTube Connector')
-    expect(Narra::Youtube::Connector.self_description).to match('Allows NARRA to connects to the YouTube sources')
+    expect(Narra::Youtube::Connector.description).to match('Allows NARRA to connects to the YouTube sources')
   end
 
   it 'should validate url' do
@@ -42,22 +42,12 @@ describe Narra::Youtube::Connector do
     expect(Narra::Youtube::Connector.valid?('https://www.youtube.com/watch?f=qM9f01YYDJ4')).to match(false)
     expect(Narra::Youtube::Connector.valid?('https://www.youtube.com/watch?v=tDyeiePort0')).to match(true)
     expect(Narra::Youtube::Connector.valid?('https://www.youtube.com/watch?v=tDyeiePort0&spfreload=10')).to match(true)
-    expect(Narra::Youtube::Connector.valid?('https://www.youtube.com/watch?v=qM9f01YYDJ4asdasdasdadasdasdasdasdaasdasdad')).to match(false) #redirect na spravnou
+    expect(Narra::Youtube::Connector.valid?('https://www.youtube.com/watch?v=qM9f01YYDJ4asdasdasdadasdasdasdasdaasdasdad')).to match(true) #redirect na spravnou
     expect(Narra::Youtube::Connector.valid?('https://www.youtube.com/watchv=tDyeiePort0')).to match(false)
     expect(Narra::Youtube::Connector.valid?('https://www.youtube.com/watch?vtDyeiePort0')).to match(false)
     expect(Narra::Youtube::Connector.valid?('http://www.youtube.com/watch?v=tDyeiePort0')).to match(true)    #redirect https://
     expect(Narra::Youtube::Connector.valid?('https:www.youtube.youtu.be.com/watch?v=tDyeiePort0')).to match(false)
   end
-
-  it 'should validate type' do
-    expect(Narra::Youtube::Connector.valid?('youtube#videoListResponse')).to match(true)
-    expect(Narra::Youtube::Connector.valid?('vimeo')).to match(false)
-    expect(Narra::Youtube::Connector.valid?('youtube')).to match(false)
-    expect(Narra::Youtube::Connector.valid?('exception')).to match(false)
-    expect(Narra::Youtube::Connector.valid?('http://www.youtube.com/watch?v=tDyeiePort0')).to match(false)
-  end
-
-  # todo how to validate rest of functions
 end
 
 
@@ -66,41 +56,44 @@ describe 'object_youtube_connector' do
   before(:each) do
     @test1 = Narra::Youtube::Connector.new("https://www.youtube.com/watch?v=5IvlcZyjJvU")
     @test2 = Narra::Youtube::Connector.new("https://www.youtube.com/watch?v=2ndeBBsQZqQ")
+    @test3 = Narra::Youtube::Connector.new("https://www.youtube.com/watch?v=U0jH2-VF00Y")
     # jak z  {name:'channelId', value:'#{@channelId}'},
-    # udělat {'channelId'=>'#{@channelId}'}               data.each
+    # udělat {'channelId'=>'#{@channelId}'}
     @data = {}
     @data1 = {}
+    @data2 = {}
     @test1.metadata.each { |i| @data[i[:name]] = i[:value] }
     @test2.metadata.each { |i| @data1[i[:name]] = i[:value] }
+    @test3.metadata.each { |i| @data2[i[:name]] = i[:value] }
   end
 
   it 'test top gear video test1' do
     expect(@test1.name).to match('Top Gear   Season 12 Episode 4 --  Series 12 Episode 4')
-    expect(@test1.type).to match('youtube#videoListResponse')
+    expect(@test1.type).to match(:video)
     expect(@test1.metadata).to be_instance_of(Array)
 
     #test channelId
-    expect(@data['channelId']).to match('UCsKSU2Og2vyMk-E6GgNH4-A') ## pak to testovat takhle !!!!!!!!!
+    expect(@data['channelId']).to match('UCsKSU2Og2vyMk-E6GgNH4-A')
     #test channelTitle
     expect(@data['channelTitle']).to match('Top Gear 12 Full HD')
     #test publishedAt
     expect(@data['publishedAt']).to match('2015-02-01T21:45:24.000Z')
     #test description
-    expect(@data['description']).to match('Tags:\nTop Gear   Season 12 Episode 4 --  Series 12 Episode 4\nTop Gear   Season 12 Full HD\n================================================Top Gear is a British television series about motor vehicles, primarily cars, and is the most widely watched factual television programme in the world.[2] It began in 1977 as a conventional motoring magazine programme. Over time, and especially since a relaunch in 2002, it has developed a quirky, humorous and sometimes controversial[3][4] style. The programme is currently presented by Jeremy Clarkson, Richard Hammond and James May, and has featured at least three different test drivers known as The Stig. The programme is estimated to have around 350 million views per week in 170 different countries.')
+    expect(@data['description']).to match("Tags:\nTop Gear   Season 12 Episode 4 --  Series 12 Episode 4\nTop Gear   Season 12 Full HD\n================================================Top Gear is a British television series about motor vehicles, primarily cars, and is the most widely watched factual television programme in the world.[2] It began in 1977 as a conventional motoring magazine programme. Over time, and especially since a relaunch in 2002, it has developed a quirky, humorous and sometimes controversial[3][4] style. The programme is currently presented by Jeremy Clarkson, Richard Hammond and James May, and has featured at least three different test drivers known as The Stig. The programme is estimated to have around 350 million views per week in 170 different countries.")
     #test categoryId
     expect(@data['categoryId']).to match('1')
     #test liveBroadcastContent
     expect(@data['liveBroadcastContent']).to match('none')
     #test viewCount
-    expect(@data['viewCount']).to match('45459')
+    expect(@data['viewCount']).to match('50277')
     #test likeCount
-    expect(@data['likeCount']).to match('111')
+    expect(@data['likeCount']).to match('120')
     #test dislikeCount
-    expect(@data['dislikeCount']).to match('7')
+    expect(@data['dislikeCount']).to match('8')
     #test favouriteCount
-    expect(@data['favouriteCount']).to match('0')
+    expect(@data['favouriteCount']).to match('')
     #test commentCount
-    expect(@data['commentCount']).to match('6')
+    expect(@data['commentCount']).to match('7')
     #test duration
     expect(@data['duration']).to match('PT1H5M47S')
     #test dimension
@@ -114,7 +107,7 @@ describe 'object_youtube_connector' do
 
   it 'test Kanalgratis video test2' do
     expect(@test2.name).to match('Kanalgratis Live - Danish Record Pike 21,1 kg, 46lb 8oz - Interview with Finn Sloth Hansen')
-    expect(@test2.type).to match('youtube#videoListResponse')
+    expect(@test2.type).to match(:video)
     expect(@test2.metadata).to be_instance_of(Array)
 
     #test channelId
@@ -129,6 +122,28 @@ describe 'object_youtube_connector' do
     expect(@data1['categoryId']).to match('17')
     #test liveBroadcastContent
     expect(@data1['liveBroadcastContent']).to match('none')
+    #test viewCount
+    expect(@data['viewCount']).to match('50277')
+    #test likeCount
+    expect(@data['likeCount']).to match('120')
+    #test dislikeCount
+    expect(@data['dislikeCount']).to match('8')
+    #test favouriteCount
+    expect(@data['favouriteCount']).to match('')
+    #test commentCount
+    expect(@data['commentCount']).to match('7')
+    #test duration
+    expect(@data['duration']).to match('PT1H5M47S')
+    #test dimension
+    expect(@data['dimension']).to match('3d')
+    #test definition
+    expect(@data['definition']).to match('hd')
+    #test caption
+    expect(@data['caption']).to match('false')
+  end
+
+  it 'test description' do
+    expect(@data2['description']).to match('I do not own this video. Nevlastním toto video. Majitel je TV Nova.')
   end
 
 end
